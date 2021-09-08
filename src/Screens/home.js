bf8e27971de3a463ddca1f3ee578dbd8068040e9
akpgaa <46481890+akpgaa@users.noreply.github.com>
2021-09-08 14:30:58 +0530

    logo change home page change

--- src/Screens/home.js
+++ src/Screens/home.js
@@ -3,9 +3,16 @@ import Essentials from '../Components/Validation';
 export default class home extends Essentials {
     render() {
         return (
-            <div>
-                HEllo
-            </div>
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="block-header" align="center">
+                        <h3 class="" style={{ color: "#F44336" }}>
+                            {" "}
+                            Welcome to Criminal Search System
+                        </h3>
+                    </div>
+                </div>
+            </section>
         )
     }
 }
